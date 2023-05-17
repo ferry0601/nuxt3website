43e55362aad2c44ac6584e6d2160fc8a930f8250
ferry0601 <wahyuferryansyah0601@gmail.com>
2023-05-17 13:23:32 +0700

    sconf

---
+++ pages/spots/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div class=" container max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-5 px-5 my-10">
+    <p class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Gambar pemandangan untuk {{ id }}</p>
+    <p class="text-gray-900 dark:text-white">akan egera ditampilkan mulai dari beberapa minggu ke depan, untuk saat ini program lagi error</p>
+  </div>
+  <TheFooter />
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+    
+</script>
+
+<style>
+
+</style>
---
+++ pages/spots/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Productts</p>
+  </div>
+
+  
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
